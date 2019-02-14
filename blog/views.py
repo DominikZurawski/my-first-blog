73e7c27bf6fb877a3c053756689375810b7a6e48
DominikZurawski <dominikzurawski15@gmail.com>
2019-02-14 22:29:08 +0100

    Dodatki

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+    latest_question_list = Post.objects.order_by('-created_date')[:5]
+    output = ', '.join([q.title for q in latest_question_list])
+    return HttpResponse(output)
