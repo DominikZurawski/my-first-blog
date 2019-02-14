67995ad50cc33cc7996077acd8ce9d1d5e597001
DominikZurawski <dominikzurawski15@gmail.com>
2019-02-14 22:58:15 +0100

    Zmieniony kod HTML na stronie.

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,5 @@ from django.utils import timezone
 
 from .models import Post
 
-def index(request):
-    latest_question_list = Post.objects.order_by('-created_date')[:5]
-    output = ', '.join([q.title for q in latest_question_list])
-    return HttpResponse(output)
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
